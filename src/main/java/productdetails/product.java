e6c102cb8bbcff27d88af0e04cb9e8dc96ee6c8a
Rahul <rahulagarwal931454@gmail.com>
2022-08-08 09:26:28 +0530

    hibernate program adding  product

---
+++ src/main/java/productdetails/product.java
@@ -0,0 +1,27 @@
+package productdetails;
+
+
+public class product {
+	private int pid;
+	private String pname;
+	private int qty;
+	public int getqty() {
+		return qty;
+	}
+	public void setqty(int qty) {
+		this.qty = qty;
+	}
+	public int getpid() {
+		return pid;
+	}
+	public void setpid(int pid) {
+		this.pid = pid;
+	}
+	public String getpname() {
+		return pname;
+	}
+	public void setpname(String pname) {
+		this.pname = pname;
+	}
+	
+}
